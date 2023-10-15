1e43dc6db38cdec940a3a29a833dc430570b2891
Web-Mastermind <AZEAgent@gmail.com>
2023-10-16 01:11:51 +0400

    Java Script Tasks

---
+++ age calculator/script.js
@@ -0,0 +1,3 @@
+let birthday = parseInt(prompt("Doğum ilinizi daxil edin"))
+let agecalculator = (2023 - birthday)
+document.write(`Sizin ${agecalculator} yaşınız var`)
---
+++ km to mil/script.js
@@ -0,0 +1,3 @@
+let km = parseInt(prompt("Km məsafəsini daxil edin"));
+let mile = km * 0.621371;
+alert(mile)
