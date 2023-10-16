7884f82b5c010969fd6196a7f0421dd9574b3c7f
Web-Mastermind <AZEAgent@gmail.com>
2023-10-16 20:53:18 +0400

    fix

--- Rectangle/script.js
+++ Rectangle/script.js
@@ -1,4 +1,4 @@
 let length = parseInt(prompt("length of a Rectangle  "));
 let width = parseInt(prompt("width of a Rectangle  "));
-let area = (length + width);
+let area = (length * width);
 document.write(`The area of a rectangle is: ${area}`)
