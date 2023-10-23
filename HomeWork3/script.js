24a3f79737698aa7047ec883a2bee28eb6e86521
Web-Mastermind <AZEAgent@gmail.com>
2023-10-24 00:13:05 +0400

    HomeWork1 folder deleted

--- HomeWork3/script.js
+++ HomeWork3/script.js
@@ -1,22 +1,12 @@
-const secretnumber = 23;
+const currentpassword = 123456;
 
-let usernumber = +prompt("Ürəyimdə tutuduğum rəqəmi tapın: 0-50 arasi");
-let usercount = 1;
+let userpassword = +prompt("Parolı daxil edin");
 
-while (usernumber !== secretnumber) {
-  usercount++;
-  usernumber = +prompt("Cavab sehvdi, Ürəyimdə tutuduğum rəqəmi tap: 0-50 arasi");
+while (userpassword !== currentpassword) {
+  userpassword = +prompt("Parol yanlışdır, Parolı daxil edin");
+ 
 }
 
-if (usercount === 1) {
-  document.write("Cavab doğrudur: Kapital Bank Hesabınıza 1000AZN köçürüldü")
-}
-else if (usercount > 1 && usercount <= 5 ) {
-  document.write("Cavab doğrudur: Kapital Bank Hesabınıza 750AZN köçürüldü")
-}
-else if (usercount >5 && usercount <=9) {
-  document.write("Cavab doğrudur: Kapital Bank Hesabınıza 250AZN köçürüldü")
-}
-else {
-  document.write("Kapital Bank Hesabınızdan -250AZN bizim hesabımıza köçürüldü")
+if (userpassword == currentpassword) {
+  document.write("Parol Doğrudur")
 }
