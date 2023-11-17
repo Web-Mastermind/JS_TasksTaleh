ce7b0af30593e9ae6202fe068fab4180bef059c5
Web-Mastermind <AZEAgent@gmail.com>
2023-11-17 19:26:29 +0400

    folder deleted

--- NameMassive/script.js
+++
@@ -1,7 +0,0 @@
-const names = ["Michael", "Trevor", "Franklin", "Lamar", "Jimmy"];
-
-function printWithIndex(name, index) {
-  console.log(`Index: ${index}, Name: ${name}`);
-}
-
-names.forEach(printWithIndex);
